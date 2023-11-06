0ce556ec4aac63098e00082a5f652d3bacfad1a1
Felliphy <107662920+Felliphy@users.noreply.github.com>
2023-11-06 09:48:05 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-/*
-  Encontre a solução ao problema:
-
-  Pergunte o nome do usuário e escreva a mensagem:
-  "Olá, [nome do usuário]"
-
-*/
-let nome = prompt('Olá, qual é o seu nome?')
-alert('Olá, ' + nome)
